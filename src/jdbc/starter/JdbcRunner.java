d8d6976af6d450d4d7e8d6d87e26eddb16b2c691
Pavel Semenov <PNSemenov@sberbank.ru>
2022-02-10 22:43:10 +0300

    added update, create example

--- src/jdbc/starter/JdbcRunner.java
+++ src/jdbc/starter/JdbcRunner.java
@@ -10,8 +10,19 @@ public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
 
         Class<Driver> driverClass = Driver.class;
+//        String sqlCreate = "create table if not exists info(" +
+//                "id serial primary key," +
+//                "data text not null )";
+ //       String sqlInsert = "insert into table ";
+        String sqlUpdate = "update flight_repository.public.info " +
+                "set data = 'TestTest' where id=5";
         try (var connection = ConnectionManager.open()) {
+            var statement = connection.createStatement();
+            System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
+            var executeResult = statement.execute(sqlUpdate);
+            System.out.println(executeResult);
+            System.out.println(statement.getUpdateCount());
         }
     }
 }
