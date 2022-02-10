d393f6dc9e369d8d339773a22f3d3a3c47353161
Pavel Semenov <PNSemenov@sberbank.ru>
2022-02-10 09:11:05 +0300

    added connection to JDBC and application.properties

---
+++ src/jdbc/starter/JdbcRunner.java
@@ -0,0 +1,17 @@
+package jdbc.starter;
+
+import jdbc.starter.util.ConnectionManager;
+import org.postgresql.Driver;
+
+import java.sql.SQLException;
+
+public class JdbcRunner {
+
+    public static void main(String[] args) throws SQLException {
+
+        Class<Driver> driverClass = Driver.class;
+        try (var connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
+    }
+}
