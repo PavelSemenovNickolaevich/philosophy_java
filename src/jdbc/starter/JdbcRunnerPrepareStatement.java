0e0a2f1ad68e142c0118ce5aba7b668b47435425
Pavel Semenov <PNSemenov@sberbank.ru>
2022-02-11 08:31:12 +0300

    added prepared statement sq; injection

---
+++ src/jdbc/starter/JdbcRunnerPrepareStatement.java
@@ -0,0 +1,68 @@
+package jdbc.starter;
+
+import jdbc.starter.util.ConnectionManager;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcRunnerPrepareStatement {
+
+    public static void main(String[] args) throws SQLException {
+//        Long flightId = 2L;
+//        var result = getTicketsByFlightId(flightId);
+//        System.out.println(result);
+        var result = getFlightsBetween(LocalDate.of(2020, 1, 1).atStartOfDay(), LocalDateTime.now());
+        System.out.println(result);
+    }
+
+    private static List<Long> getFlightsBetween(LocalDateTime start, LocalDateTime end) throws SQLException {
+//        String sql = """
+//                SELECT id
+//                FROM flight
+//                WHERE departure_date BETWEEN ? AND ?
+//                """;
+        String sql = " SELECT id FROM flight_repository.public.flight WHERE departure_date between ? and ?";
+        List<Long> result = new ArrayList<>();
+        try (var connection = ConnectionManager.open();
+             var preparedStatement = connection.prepareStatement(sql)) {
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(start));
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(end));
+            System.out.println(preparedStatement);
+
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                result.add(resultSet.getLong("id"));
+            }
+        }
+
+        return result;
+    }
+
+    private static List<Long> getTicketsByFlightId(Long flightId) throws SQLException {
+//        String sql = """
+//                SELECT id
+//                FROM ticket
+//                WHERE flight_id = ?
+//                """;
+        String sql = " SELECT id FROM flight_repository.public.ticket WHERE flight_id = ?";
+        List<Long> result = new ArrayList<>();
+        try (var connection = ConnectionManager.open();
+             var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, flightId);
+
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+//                result.add(resultSet.getLong("id"));
+                result.add(resultSet.getObject("id", Long.class)); // NULL safe
+            }
+        }
+
+        return result;
+    }
+}
