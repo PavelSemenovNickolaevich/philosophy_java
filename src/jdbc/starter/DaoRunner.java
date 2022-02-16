1223e51b6587bb7f1d06f26200554ec3f5a1b95e
Pavel Semenov <PNSemenov@sberbank.ru>
2022-02-16 08:52:01 +0300

    added UPDATE and SELECT

--- src/jdbc/starter/DaoRunner.java
+++ src/jdbc/starter/DaoRunner.java
@@ -8,9 +8,24 @@ import java.math.BigDecimal;
 public class DaoRunner {
 
     public static void main(String[] args) {
-        saveTest();
+     //   saveTest();
     //    deleteTest(59L);
+       var tickets = TicketDao.getInstance().findAll();
+      //  var ticket = TicketDao.getInstance().findById(2L);
+        System.out.println(tickets);
     }
+
+    private static void updateTest() {
+        var ticketDao = TicketDao.getInstance();
+        var maybeTicket = ticketDao.findById(2L);
+        System.out.println(maybeTicket);
+
+        maybeTicket.ifPresent(ticket -> {
+            ticket.setCost(BigDecimal.valueOf(188.88));
+            ticketDao.update(ticket);
+        });
+    }
+
 
     private static void deleteTest(Long id) {
         var ticketDao = TicketDao.getInstance();
