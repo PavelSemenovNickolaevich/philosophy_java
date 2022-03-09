37488dcabeb52092d219e68c0ce2d58efb07780b
Pavel Semenov <PNSemenov@sberbank.ru>
2022-03-09 08:25:35 +0300

    added simple socket server and client

--- src/servlets/http/socket/SocketRunner.java
+++ src/servlets/http/socket/SocketRunner.java
@@ -6,16 +6,24 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Inet4Address;
 import java.net.Socket;
+import java.util.Scanner;
+
+
+//Пример создания клиента
 
 public class SocketRunner {
     public static void main(String[] args) throws IOException {
-        var inetAddress = Inet4Address.getByName("google.com");
-        try (var socket = new Socket(inetAddress, 80);
+        // var inetAddress = Inet4Address.getByName("google.com");
+        var inetAddress = Inet4Address.getByName("localhost");
+        try (var socket = new Socket(inetAddress, 7777);
              var outputStream = new DataOutputStream(socket.getOutputStream());
-             var inputStream = new DataInputStream(socket.getInputStream())) {
-            outputStream.writeUTF("Hello World!");
-            var response = inputStream.readAllBytes();
-            System.out.println(response.length);
+             var inputStream = new DataInputStream(socket.getInputStream());
+             var scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                var request = scanner.nextLine();
+                outputStream.writeUTF(request);
+                System.out.println("Response from server: " + inputStream.readUTF());
+            }
         }
     }
 }
