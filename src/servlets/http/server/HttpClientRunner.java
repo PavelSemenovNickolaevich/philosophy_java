71df406ee6e483142c2ee693da587218b0a3d3ed
Pavel Semenov <PNSemenov@sberbank.ru>
2022-03-10 23:04:46 +0300

    added single threaded server

--- src/servlets/http/server/HttpClientRunner.java
+++ src/servlets/http/server/HttpClientRunner.java
@@ -1,32 +1,32 @@
-package servlets.http.client;
+package servlets.http.server;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
-public class HttpClientExample {
+import static java.net.http.HttpRequest.BodyPublishers.*;
+
+public class HttpClientRunner {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+
         var httpClient = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
 
-        HttpRequest request = HttpRequest.newBuilder(URI.create("https://www.google.com"))
-                .GET()
-                .build();
-
-        HttpRequest request2 = HttpRequest.newBuilder(URI.create("https://www.google.com"))
-                .POST(HttpRequest.BodyPublishers.ofFile(Path.of("path", "to", "file")))
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:9000"))
+                .header("content-type", "application/json")
+                .POST(ofFile(Path.of("resources", "first.json")))
                 .build();
 
         var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
         System.out.println(response.headers());
+        System.out.println(response.body());
     }
 }
