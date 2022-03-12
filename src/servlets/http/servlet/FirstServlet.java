f2e66e35edc66bfd314e7ac8aa80b1234532f69b
Pavel Semenov <PNSemenov@sberbank.ru>
2022-03-12 15:04:15 +0300

    added first example with servlet

---
+++ src/servlets/http/servlet/FirstServlet.java
@@ -0,0 +1,32 @@
+package servlets.http.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try(var writer = resp.getWriter()) {
+            writer.write("Hello from First Servlet");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
