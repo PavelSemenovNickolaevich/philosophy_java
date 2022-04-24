c50cb195d1dfe00f114e94045754bbc7b18d3d27
Pavel Semenov <PNSemenov@sberbank.ru>
2022-04-25 00:05:20 +0300

    added simple architecture mvc

---
+++ src/servlets/http/entity/FlightStatus.java
@@ -0,0 +1,8 @@
+package servlets.http.entity;
+
+public enum FlightStatus {
+    ARRIVED,
+    DEPARTED,
+    CANCELLED,
+    SCHEDULED
+}
---
+++ src/servlets/http/service/FlightService.java
@@ -0,0 +1,32 @@
+package servlets.http.service;
+
+import servlets.http.dao.FlightDao;
+import servlets.http.dto.FlightDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlightService {
+
+    private static final FlightService INSTANCE = new FlightService();
+    private final FlightDao flightDao = FlightDao.getInstance();
+
+    private FlightService() {
+    }
+
+    public List<FlightDto> findAll() {
+        return flightDao.findAll().stream()
+                .map(flight -> new FlightDto(
+                        flight.getId(),
+                        """
+                                %s - %s - %s
+                                """.formatted(flight.getDepartureAirportCode(), flight.getArrivalAirportCode(), flight.getStatus())
+                ))
+                .collect(Collectors.toList());
+
+    }
+
+    public static FlightService getInstance() {
+        return INSTANCE;
+    }
+}
